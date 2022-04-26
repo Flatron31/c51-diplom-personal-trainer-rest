8163dc221583e75959ce5bd7d6fa45a6373c67cf
Flatron31 <Akvadivchik@gmail.com>
2022-04-26 15:08:18 +0300

    add User

---
+++ src/main/java/com/example/c51diplompersonaltrainerrest/C51DiplomPersonalTrainerRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.c51diplompersonaltrainerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C51DiplomPersonalTrainerRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C51DiplomPersonalTrainerRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/c51diplompersonaltrainerrest/C51DiplomPersonalTrainerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c51diplompersonaltrainerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C51DiplomPersonalTrainerRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
