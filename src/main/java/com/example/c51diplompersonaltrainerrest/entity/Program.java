fc54d9513fcd61e195a9d8c9f5b8e6ab85f7bd7c
Flatron31 <Akvadivchik@gmail.com>
2022-05-01 21:49:53 +0300

    fix Program

--- src/main/java/com/example/c51diplompersonaltrainerrest/entity/Program.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/entity/Program.java
@@ -1,4 +1,28 @@
 package com.example.c51diplompersonaltrainerrest.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Program {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private List<Exercise> exerciseList;
+
+    private List<SportsSupplement> sportsSupplements;
+
+
 }
