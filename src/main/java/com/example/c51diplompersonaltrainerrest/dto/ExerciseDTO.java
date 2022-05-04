5aefa2fb8c712870bcf7b6a50b551019192a1455
Flatron31 <Akvadivchik@gmail.com>
2022-05-04 21:23:15 +0300

    add ExerciseDTO

--- src/main/java/com/example/c51diplompersonaltrainerrest/dto/ExerciseDTO.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/dto/ExerciseDTO.java
@@ -1,4 +1,26 @@
 package com.example.c51diplompersonaltrainerrest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExerciseDTO {
+
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @Size(min = 3)
+    private String description;
+
+    @Min(value = 1)
+    private long numberOfApproaches;
+
+    @Min(value = 1)
+    private long numberOfTimes;
 }
