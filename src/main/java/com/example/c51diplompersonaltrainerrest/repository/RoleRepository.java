b2f0808f03434fa0bd162c74fb9398dbe2a7813d
Flatron31 <Akvadivchik@gmail.com>
2022-05-04 23:23:18 +0300

    adding adding annotations RoleRepository

--- src/main/java/com/example/c51diplompersonaltrainerrest/repository/RoleRepository.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/repository/RoleRepository.java
@@ -2,9 +2,13 @@ package com.example.c51diplompersonaltrainerrest.repository;
 
 import com.example.c51diplompersonaltrainerrest.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Component
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
