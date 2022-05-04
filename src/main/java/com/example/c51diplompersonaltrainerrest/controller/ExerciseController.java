1f0f608736f13a505e7a4dbcfdc4be3a6eeed6af
Flatron31 <Akvadivchik@gmail.com>
2022-05-04 22:26:54 +0300

    fix ExerciseController

--- src/main/java/com/example/c51diplompersonaltrainerrest/controller/ExerciseController.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/controller/ExerciseController.java
@@ -1,10 +1,13 @@
 package com.example.c51diplompersonaltrainerrest.controller;
 
+import com.example.c51diplompersonaltrainerrest.dto.ExerciseDTO;
+import com.example.c51diplompersonaltrainerrest.Mapper.ExerciseMapper;
 import com.example.c51diplompersonaltrainerrest.entity.Exercise;
 import com.example.c51diplompersonaltrainerrest.exception.InvalidParametrException;
 import com.example.c51diplompersonaltrainerrest.exception.NotFoundException;
 import com.example.c51diplompersonaltrainerrest.repository.ExerciseRepository;
 import com.example.c51diplompersonaltrainerrest.repository.UserRepository;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
@@ -18,14 +21,15 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/v1/exercise")
+@Api(tags = "Exercise", description = "Access to posts")
+@RequestMapping("/api/user/exercise")
 public class ExerciseController {
 
     @Autowired
     private ExerciseRepository exerciseRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    private ExerciseMapper exerciseMapper;
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful operation"),
@@ -48,20 +52,15 @@ public class ExerciseController {
     @ApiResponse(responseCode = "405", description = "Invalid input")
     @ApiOperation(value = "Creating a new exercise", notes = "This can only be done by the logged in user",
             authorizations = {@Authorization(value = "apiKey")})
-    @PostMapping(value = "/{userId}", produces = "application/json")
-    public ResponseEntity<Exercise> createExercize(@ApiParam(value = "UserId is required to get a user for this id",
-            example = "1")
-                                                   @PathVariable("userId") Long userId,
-                                                   @Valid @RequestBody Exercise exercise, BindingResult bindingResult) {
-        if (userId < 1 | userRepository.findById(userId).isEmpty()) {
-            throw new NotFoundException();
-        }
+    @PostMapping(value = "", produces = "application/json")
+    public ResponseEntity<Exercise> createExercize(@ApiParam(value = "New object exercise", name = "body exercise")
+                                                   @Valid @RequestBody ExerciseDTO exerciseDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new InvalidParametrException();
         }
-        Exercise saveExercize = exerciseRepository.save(exercise);
+        Exercise exercise = exerciseMapper.exerciseDTOToExercise(exerciseDTO);
 
-        return ResponseEntity.ok(saveExercize);
+        return ResponseEntity.ok(exerciseRepository.save(exercise));
     }
 
     @ApiResponses(value = {
@@ -70,15 +69,10 @@ public class ExerciseController {
     })
     @ApiOperation(value = "Removal exercise", notes = "This can only be done by the logged in user",
             authorizations = {@Authorization(value = "apiKey")})
-    @DeleteMapping(value = "/{userId}/{id}", produces = "application/json")
-    public void deleteComment(@ApiParam(value = "UserId is required to get a user for this id", example = "1")
-                              @PathVariable("userId") Long userId,
-                              @ApiParam(value = "Id is required to receive a comment on this id", example = "1")
+    @DeleteMapping(value = "/{id}", produces = "application/json")
+    public void deleteComment(@ApiParam(value = "Id is required to receive a comment on this id", example = "1")
                               @PathVariable("id") Long id) {
-        if (userId < 1 | userRepository.findById(userId).isEmpty()) {
-            throw new NotFoundException();
-        }
-        if (userId < 1 | exerciseRepository.findById(id).isEmpty()) {
+        if (id < 1 | exerciseRepository.findById(id).isEmpty()) {
             throw new NotFoundException();
         }
         exerciseRepository.delete(exerciseRepository.getById(id));
@@ -91,26 +85,21 @@ public class ExerciseController {
     })
     @ApiOperation(value = "Updated exercise", notes = "This can only be done by the logged in user",
             authorizations = {@Authorization(value = "apiKey")})
-    @PutMapping(value = "/{userId}/{id}", produces = "application/json")
-    public ResponseEntity<Exercise> updateComment(@ApiParam(value = "UserId is required to get a user for this id", example = "1")
-                                                  @PathVariable("userId") Long userId,
-                                                  @ApiParam(value = "Id is required to receive a comment on this id", example = "1")
+    @PutMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<Exercise> updateComment(@ApiParam(value = "The identifier is required to get the exercise " +
+            "for this id for subsequent changes", example = "1")
                                                   @PathVariable("id") Long id,
                                                   @ApiParam(value = "Creating a modified Exercise object", name = "body exercise")
                                                   @Valid @RequestBody Exercise exercise, BindingResult bindingResult) {
-        if (userId < 1 | userRepository.findById(userId).isEmpty()) {
-            throw new NotFoundException();
-        }
-        if (userId < 1 | exerciseRepository.findById(id).isEmpty()) {
+        if (id < 1 | exerciseRepository.findById(id).isEmpty()) {
             throw new NotFoundException();
         }
         if (bindingResult.hasErrors()) {
             throw new InvalidParametrException();
         }
         exercise.setId(id);
-        Exercise saveExercise = exerciseRepository.save(exercise);
 
-        return ResponseEntity.ok(saveExercise);
+        return ResponseEntity.ok(exerciseRepository.save(exercise));
     }
 }
 
