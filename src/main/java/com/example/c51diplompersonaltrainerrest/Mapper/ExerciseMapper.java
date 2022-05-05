25d3a7b9efbedbbe518daf2b3b04484452c843fd
Flatron31 <Akvadivchik@gmail.com>
2022-05-05 10:11:30 +0300

    adding annotation @Component

--- src/main/java/com/example/c51diplompersonaltrainerrest/Mapper/ExerciseMapper.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/Mapper/ExerciseMapper.java
@@ -3,7 +3,9 @@ package com.example.c51diplompersonaltrainerrest.Mapper;
 import com.example.c51diplompersonaltrainerrest.dto.ExerciseDTO;
 import com.example.c51diplompersonaltrainerrest.entity.Exercise;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface ExerciseMapper {
     ExerciseDTO exerciseDTOToExercise(Exercise exercise);
--- src/main/java/com/example/c51diplompersonaltrainerrest/Mapper/SportsNutritionMapper.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/Mapper/SportsNutritionMapper.java
@@ -3,7 +3,9 @@ package com.example.c51diplompersonaltrainerrest.Mapper;
 import com.example.c51diplompersonaltrainerrest.dto.SportsNutritionDTO;
 import com.example.c51diplompersonaltrainerrest.entity.SportsNutrition;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface SportsNutritionMapper {
     SportsNutritionDTO SportsNutritionDTOToSportsNutrition(SportsNutrition sportsNutrition);
