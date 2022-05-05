43aed3eda40afe479151018d75e6bd6261d7833e
Flatron31 <Akvadivchik@gmail.com>
2022-05-05 10:10:11 +0300

    adding a SportsNutritionDTO

---
+++ src/main/java/com/example/c51diplompersonaltrainerrest/dto/SportsNutritionDTO.java
@@ -0,0 +1,25 @@
+package com.example.c51diplompersonaltrainerrest.dto;
+
+import com.example.c51diplompersonaltrainerrest.entity.SportsSupplement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SportsNutritionDTO {
+
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @Size(min = 3)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private SportsSupplement sportsSupplement;
+
+}
