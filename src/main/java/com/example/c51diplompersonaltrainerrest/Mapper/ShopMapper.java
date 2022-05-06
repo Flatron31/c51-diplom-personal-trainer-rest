b0046597ce850ea1835ffa0a33d33c28b45e7aa7
Flatron31 <Akvadivchik@gmail.com>
2022-05-06 10:27:38 +0300

    adding а ShopMapper

---
+++ src/main/java/com/example/c51diplompersonaltrainerrest/Mapper/ShopMapper.java
@@ -0,0 +1,13 @@
+package com.example.c51diplompersonaltrainerrest.Mapper;
+
+import com.example.c51diplompersonaltrainerrest.dto.ShopDTO;
+import com.example.c51diplompersonaltrainerrest.entity.Shop;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = "spring")
+public interface ShopMapper {
+    ShopDTO shopToShopDTO(Shop shop);
+    Shop shopDTOToShop(ShopDTO shopDTO);
+}
