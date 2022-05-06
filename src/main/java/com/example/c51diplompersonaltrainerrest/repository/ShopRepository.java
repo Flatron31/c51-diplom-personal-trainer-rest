271e4a87ca93fcbd30daa32e1cd126d94194cb7a
Flatron31 <Akvadivchik@gmail.com>
2022-05-06 10:41:34 +0300

    adding a ShopRepository

--- src/main/java/com/example/c51diplompersonaltrainerrest/repository/ShopRepository.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/repository/ShopRepository.java
@@ -1,9 +1,11 @@
 package com.example.c51diplompersonaltrainerrest.repository;
 
+import com.example.c51diplompersonaltrainerrest.entity.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Component
 @Repository
-public interface ShopRepository {
+public interface ShopRepository extends JpaRepository<Shop, Long> {
 }
