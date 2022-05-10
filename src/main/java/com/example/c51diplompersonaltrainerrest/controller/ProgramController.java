6aabed9164b2fa9ff68679e56c95e8eaefb1a1a6
Flatron31 <Akvadivchik@gmail.com>
2022-05-10 22:42:54 +0300

    added annotations for swagger

--- src/main/java/com/example/c51diplompersonaltrainerrest/controller/ProgramController.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/controller/ProgramController.java
@@ -8,6 +8,11 @@ import com.example.c51diplompersonaltrainerrest.repository.ProgramRepository;
 import com.example.c51diplompersonaltrainerrest.repository.UserRepository;
 import com.example.c51diplompersonaltrainerrest.service.ProgramService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Authorization;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,9 +38,17 @@ public class ProgramController {
         this.programRepository = programRepository;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
+    @ApiOperation(value = "Creating a program for the user under the given id",
+            authorizations = {@Authorization(value = "apiKey")})
     @PostMapping("/{id}")
-    public ResponseEntity<Program> createProgramForUser(@PathVariable ("id") long id) {
-        if (id < 1 | userRepository.findById(id).isEmpty()){
+    public ResponseEntity<Program> createProgramForUser(@ApiParam(value = "This id is required to search for " +
+            "a user under this id", example = "1")
+                                                        @PathVariable("id") long id) {
+        if (id < 1 | userRepository.findById(id).isEmpty()) {
             throw new NotFoundException();
         }
         User user = userRepository.getById(id);
@@ -49,9 +62,16 @@ public class ProgramController {
         return ResponseEntity.ok(save);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
+    @ApiOperation(value = "Getting all user programs by id", authorizations = {@Authorization(value = "apiKey")})
     @GetMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<List<Program>> getAllProgramUser(@PathVariable ("id") long id) {
-        if (id < 1 | userRepository.findById(id).isEmpty()){
+    public ResponseEntity<List<Program>> getAllProgramUser(@ApiParam(value = "This id is required to search for " +
+            "a user under this id", example = "1")
+                                                           @PathVariable("id") long id) {
+        if (id < 1 | userRepository.findById(id).isEmpty()) {
             throw new NotFoundException();
         }
         User user = userRepository.getById(id);
