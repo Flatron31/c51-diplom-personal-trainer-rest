fc7343493a3242facb471cd53112d4cdc4aabf4f
Flatron31 <Akvadivchik@gmail.com>
2022-05-11 22:58:17 +0300

    added @NonNull

--- src/main/java/com/example/c51diplompersonaltrainerrest/entity/SportsNutrition.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/entity/SportsNutrition.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
@@ -32,6 +33,7 @@ public class SportsNutrition {
     @ManyToMany
     private List<Program> listProgram;
 
+    @NonNull
     @Enumerated(EnumType.STRING)
     private SportsSupplement sportsSupplement;
 }
