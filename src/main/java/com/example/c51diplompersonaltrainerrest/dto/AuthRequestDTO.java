42542469b6df05704c2eceb7fc3027f1dd514d67
Flatron31 <Akvadivchik@gmail.com>
2022-05-11 23:03:20 +0300

    added @NonNull

--- src/main/java/com/example/c51diplompersonaltrainerrest/dto/AuthRequestDTO.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/dto/AuthRequestDTO.java
@@ -3,11 +3,16 @@ package com.example.c51diplompersonaltrainerrest.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequestDTO {
+
+    @NonNull
     private String username;
+
+    @NonNull
     private String password;
 }
