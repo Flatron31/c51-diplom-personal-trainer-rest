e55562c32f6331633c765881904f668ad0ad2d79
Flatron31 <Akvadivchik@gmail.com>
2022-05-12 00:21:45 +0300

    added @ApiResponse for Swagger

--- src/main/java/com/example/c51diplompersonaltrainerrest/controller/AuthentificationController.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/controller/AuthentificationController.java
@@ -4,7 +4,12 @@ import com.example.c51diplompersonaltrainerrest.configuration.security.Jwt.JwtTo
 import com.example.c51diplompersonaltrainerrest.dto.AuthRequestDTO;
 import com.example.c51diplompersonaltrainerrest.dto.UserDTO;
 import com.example.c51diplompersonaltrainerrest.entity.User;
+import com.example.c51diplompersonaltrainerrest.exception.InvalidParametrException;
 import com.example.c51diplompersonaltrainerrest.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +18,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,8 +42,41 @@ public class AuthentificationController {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "405", description = "Invalid input")
+    })
+    @ApiOperation(value = "User registration")
+    @PostMapping("/reg")
+    public ResponseEntity<UserDTO> registration(@ApiParam(value = "Create a new user object", example = "userDTO")
+                                                @Valid @RequestBody UserDTO userDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidParametrException();
+        }
+        if (service.existByUsername(userDTO.getUsername()) || service.existByEmail(userDTO.getEmail())) {
+            log.info("{} not registered", userDTO.getUsername());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        service.registration(userDTO);
+
+        log.info("Registered successfully {}", userDTO.getUsername());
+
+        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "405", description = "Invalid input")
+    })
+    @ApiOperation(value = "User authorization")
     @PostMapping("/login")
-    public ResponseEntity<Map<Object, Object>> logIn(@RequestBody AuthRequestDTO requestDto) {
+    public ResponseEntity<Map<Object, Object>> logIn(@ApiParam(value = "Authorization object", example = "requestDto")
+                                                     @Valid @RequestBody AuthRequestDTO requestDto,
+                                                     BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidParametrException();
+        }
 
         String username = requestDto.getUsername();
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, requestDto.getPassword()));
@@ -49,23 +89,10 @@ public class AuthentificationController {
         resp.put("token", token);
 
         log.info("Authorized {}", username);
-        log.error("{} not authorized", username);
 
         return new ResponseEntity<>(resp, HttpStatus.OK);
     }
 
-    @PostMapping("/reg")
-    public ResponseEntity<UserDTO> registration(@RequestBody UserDTO userDTO) {
-        if (service.existByUsername(userDTO.getUsername()) || service.existByEmail(userDTO.getEmail())) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        service.registration(userDTO);
-
-        log.info("Registered successfully {}", userDTO.getUsername());
-        log.error("{} not registered", userDTO.getUsername());
-
-        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
-    }
 
     @GetMapping("/logout")
     public ResponseEntity<Map<Object, Object>> logOut(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
