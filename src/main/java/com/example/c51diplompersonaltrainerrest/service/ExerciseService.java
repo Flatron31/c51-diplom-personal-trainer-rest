b5198e0697151991bb333dc7fb493682d0294fa6
Flatron31 <Akvadivchik@gmail.com>
2022-06-09 12:22:04 +0300

    fix methods and added ExerciseService

---
+++ src/main/java/com/example/c51diplompersonaltrainerrest/service/ExerciseService.java
@@ -0,0 +1,29 @@
+package com.example.c51diplompersonaltrainerrest.service;
+
+import com.example.c51diplompersonaltrainerrest.dto.ExerciseDTO;
+import com.example.c51diplompersonaltrainerrest.entity.Exercise;
+import com.example.c51diplompersonaltrainerrest.mapper.ExerciseMapper;
+import com.example.c51diplompersonaltrainerrest.repository.ExerciseRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ExerciseService {
+
+    private final ExerciseRepository exerciseRepository;
+    private final ExerciseMapper exerciseMapper;
+
+    public ExerciseService(ExerciseRepository exerciseRepository,
+                           ExerciseMapper exerciseMapper) {
+        this.exerciseRepository = exerciseRepository;
+        this.exerciseMapper = exerciseMapper;
+    }
+
+    public void createExercise(ExerciseDTO exerciseDTO){
+        Exercise exercise = exerciseMapper.exerciseDTOToExercise(exerciseDTO);
+        log.info("New exercise {} added", exerciseDTO.getName());
+        exerciseRepository.save(exercise);
+    }
+
+}
