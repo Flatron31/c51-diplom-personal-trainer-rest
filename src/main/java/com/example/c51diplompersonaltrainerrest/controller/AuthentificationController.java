757b94e87f704dd6146a8e281fddb94518e4f947
Flatron31 <Akvadivchik@gmail.com>
2022-06-10 12:43:35 +0300

    added validator

--- src/main/java/com/example/c51diplompersonaltrainerrest/controller/AuthentificationController.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/controller/AuthentificationController.java
@@ -4,10 +4,11 @@ import com.example.c51diplompersonaltrainerrest.configuration.security.Jwt.JwtTo
 import com.example.c51diplompersonaltrainerrest.dto.AuthRequestDTO;
 import com.example.c51diplompersonaltrainerrest.dto.UserDTO;
 import com.example.c51diplompersonaltrainerrest.entity.User;
-import com.example.c51diplompersonaltrainerrest.exception.InvalidParametrException;
 import com.example.c51diplompersonaltrainerrest.service.UserService;
+import com.example.c51diplompersonaltrainerrest.validation.Validator;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Authorization;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
@@ -35,11 +36,16 @@ public class AuthentificationController {
     private final UserService service;
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
+    private final Validator validator;
 
-    public AuthentificationController(UserService service, AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+    public AuthentificationController(UserService service,
+                                      AuthenticationManager authenticationManager,
+                                      JwtTokenProvider jwtTokenProvider,
+                                      Validator validator) {
         this.service = service;
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.validator = validator;
     }
 
     @ApiResponses(value = {
@@ -48,12 +54,10 @@ public class AuthentificationController {
             @ApiResponse(responseCode = "405", description = "Invalid input")
     })
     @ApiOperation(value = "User registration")
-    @PostMapping("/reg")
+    @PostMapping(value = "/reg")
     public ResponseEntity<UserDTO> registration(@ApiParam(value = "Create a new user object", example = "userDTO")
                                                 @Valid @RequestBody UserDTO userDTO, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            throw new InvalidParametrException();
-        }
+        validator.validate(bindingResult);
         if (service.existByUsername(userDTO.getUsername()) || service.existByEmail(userDTO.getEmail())) {
             log.info("{} not registered", userDTO.getUsername());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -70,13 +74,11 @@ public class AuthentificationController {
             @ApiResponse(responseCode = "405", description = "Invalid input")
     })
     @ApiOperation(value = "User authorization")
-    @PostMapping("/login")
+    @PostMapping(value = "/login")
     public ResponseEntity<Map<Object, Object>> logIn(@ApiParam(value = "Authorization object", example = "requestDto")
                                                      @Valid @RequestBody AuthRequestDTO requestDto,
                                                      BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            throw new InvalidParametrException();
-        }
+        validator.validate(bindingResult);
 
         String username = requestDto.getUsername();
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, requestDto.getPassword()));
@@ -93,8 +95,13 @@ public class AuthentificationController {
         return new ResponseEntity<>(resp, HttpStatus.OK);
     }
 
-
-    @GetMapping("/logout")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "403", description = "Forbidden")
+    })
+    @ApiOperation(value = "Logout user", notes = "This can only be done by the logged in user",
+            authorizations = {@Authorization(value = "apiKey")})
+    @GetMapping(value = "/logout")
     public ResponseEntity<Map<Object, Object>> logOut(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Map<Object, Object> resp = new HashMap<>();
