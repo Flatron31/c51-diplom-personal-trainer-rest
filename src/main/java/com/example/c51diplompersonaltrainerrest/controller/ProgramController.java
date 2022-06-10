432875b9e6ffe70f1d18f0de4046671053b8fe0c
Flatron31 <Akvadivchik@gmail.com>
2022-06-10 12:44:47 +0300

    fix createProgram

--- src/main/java/com/example/c51diplompersonaltrainerrest/controller/ProgramController.java
+++ src/main/java/com/example/c51diplompersonaltrainerrest/controller/ProgramController.java
@@ -2,8 +2,6 @@ package com.example.c51diplompersonaltrainerrest.controller;
 
 import com.example.c51diplompersonaltrainerrest.entity.Program;
 import com.example.c51diplompersonaltrainerrest.entity.User;
-import com.example.c51diplompersonaltrainerrest.exception.NotFoundException;
-import com.example.c51diplompersonaltrainerrest.mapper.UserMapper;
 import com.example.c51diplompersonaltrainerrest.repository.ProgramRepository;
 import com.example.c51diplompersonaltrainerrest.repository.UserRepository;
 import com.example.c51diplompersonaltrainerrest.service.ProgramService;
@@ -21,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 @Api(tags = "Program", description = "Operations with the program object")
 @RequestMapping("/api/user/program")
@@ -30,15 +27,12 @@ public class ProgramController {
     private final UserRepository userRepository;
     private final ProgramService programService;
     private final ProgramRepository programRepository;
-    private final Validator validator;
     private final UserService userService;
 
-    public ProgramController(Validator validator,
-                             UserRepository userRepository,
+    public ProgramController(UserRepository userRepository,
                              ProgramService programService,
                              ProgramRepository programRepository,
                              UserService userService) {
-        this.validator = validator;
         this.userRepository = userRepository;
         this.programService = programService;
         this.programRepository = programRepository;
@@ -58,13 +52,7 @@ public class ProgramController {
                                                         @PathVariable("id") long id) {
         userService.validateUserId(id);
 
-        User user = userRepository.getById(id);
-        List<Program> programList = user.getProgramList();
-        Program program = programService.createProgram(user);
-        programList.add(program);
-        user.setProgramList(programList);
-        programRepository.save(program);
-        log.info("New program added {}", user.getUsername());
+        programService.createProgram(id);
     }
 
     @ApiResponses(value = {
